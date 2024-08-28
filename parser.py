5a51babebf2602e4b614f766ca95cb0b94ac36a5
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-08-28 16:06:41 +0100

    added more context free grammer rules from lecture source code in NON-TERMINALS global variable.

--- parser.py
+++ parser.py
@@ -17,7 +17,12 @@ V -> "smiled" | "tell" | "were"
 """
 
 NONTERMINALS = """
-S -> N V
+    S -> NP VP
+
+    AP -> A | A AP
+    NP -> N | D NP | AP NP | N PP
+    PP -> P NP
+    VP -> V | V NP | V NP PP
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
